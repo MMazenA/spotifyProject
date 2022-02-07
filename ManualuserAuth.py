6ea4b910b7b95974f6659b412a5303c0cb8d61f2
MMazenA <Jazenjirza@gmail.com>
2022-02-06 23:21:21 -0500

    Hidden API codes / cleanup

--- ManualuserAuth.py
+++ ManualuserAuth.py
@@ -2,48 +2,39 @@ from flask import Flask, redirect, request
 from urllib import request
 from hyperlink import URL
 import requests
+import privateInfo   # file containing client codes
 
 
-#client id and secret
-client_id='aa1826bc005040e98502bf7d9e6d5ba2'    
-secret_id='.'
+# client id and secret
+client_id = privateInfo.clientID()
+secret_id = privateInfo.secretID()
 
 
-
-#authentication URL 
-userauthenticate=(
+# authentication URL
+userauthenticate = (
     "response_type=code" +
     '&client_id='+client_id +
     "&scope=user-read-currently-playing%20user-read-playback-state%20user-read-playback-position"
-    "&redirect_uri=http://127.0.0.1:5000/home/" 
-    
+    "&redirect_uri=http://127.0.0.1:5000/home/"
+
 )
 
 
+# authentication using flask for browser
+app = Flask(__name__)
 
-
-#authentication using flask for browser
-app= Flask(__name__)
 
 def authenticate():
 
     @app.route("/")
     def spotify():
-        return redirect('https://accounts.spotify.com/authorize?'+userauthenticate)    
-
+        return redirect('https://accounts.spotify.com/authorize?'+userauthenticate)
 
     @app.route("/home/")
     def home():
         return ('hi')
 
-    app.run()
-
-
-
-#https://accounts.spotify.com/authorize?${query}
+    app.run(host='0.0.0.0')
 
 
 authenticate()
-
-
-
--- spotify.py
+++ spotify.py
@@ -1,4 +1,5 @@
 
+from ast import Str
 from os import access
 from re import S
 from urllib import response
@@ -7,91 +8,105 @@ from pprint import pprint
 import base64
 from urllib.parse import urlencode
 import time
+import privateInfo  # file containing client codes
 
 
-#provides accesstoken through refreshtoken recieved from manualuserAuth
 def getAccessToken():
-    client_id='aa1826bc005040e98502bf7d9e6d5ba2'
-    secret_id='.'
+    client_id = privateInfo.clientID()
+    secret_id = privateInfo.secretID()
 
-    token_url="https://accounts.spotify.com/api/token"
+    token_url = "https://accounts.spotify.com/api/token"
 
-    token_data={
-        "grant_type": "authorization_code",
-        "code":".",                                 
-        "redirect_uri":"http://127.0.0.1:5000/home/"
+    token_data_refresh = {
+        "grant_type": "refresh_token",
+        "refresh_token": privateInfo.refreshToken(),
+        "redirect_uri": "http://127.0.0.1:5000/home/"
+    }
+    client_creds = f"{client_id}:{secret_id}"
+    client_creds_b64 = base64.b64encode(client_creds.encode())
+
+    token_headers = {
+        "Authorization": f"Basic {client_creds_b64.decode()}"
     }
 
-    token_data_refresh={
-        "grant_type": "refresh_token",
-        "refresh_token":".",                        
-        "redirect_uri":"http://127.0.0.1:5000/home/"
-        }
-    client_creds = f"{client_id}:{secret_id}"
-    client_creds_b64 =base64.b64encode(client_creds.encode())
+    r = requests.post(token_url, data=token_data_refresh,
+                      headers=token_headers)
+    resp_json = r.json()
+    requested_access_token = resp_json['access_token']
+    # print(r.json())
+    # print(requested_access_token)
+    # s_access_token=s_access_token= 'BQCsGhfymKzbYVXf_dP8M4OaUooU5DDQiS5Eqec7RPGGuBMBUxiqewQlWFKLS-8ygf4-4x1hBPkHvCIMmdQ_6GfJISdpbPOtSdUPSezFSxmOUvSkmD3ZcU8MZJhTC8kVAAE4zzxVCsVrZF6WHnYpkieoMw'
 
-    token_headers={ 
-            "Authorization": f"Basic {client_creds_b64.decode()}"
-        }
-
-    r=requests.post(token_url,data=token_data_refresh,headers=token_headers)
-    resp_json=r.json()
-    requested_access_token=resp_json['access_token']
-    #print(r.json())
-    #s_access_token=s_access_token= 'BQCsGhfymKzbYVXf_dP8M4OaUooU5DDQiS5Eqec7RPGGuBMBUxiqewQlWFKLS-8ygf4-4x1hBPkHvCIMmdQ_6GfJISdpbPOtSdUPSezFSxmOUvSkmD3ZcU8MZJhTC8kVAAE4zzxVCsVrZF6WHnYpkieoMw'
     return requested_access_token
-    
-    
+
 
 def current_track(access_token):
-    current_track_url='https://api.spotify.com/v1/me/player'
-    response=requests.get(
-    current_track_url,
-    headers={
+    current_track_url = 'https://api.spotify.com/v1/me/player'
+    response = requests.get(
+        current_track_url,
+        headers={
             "Authorization": f"Bearer {access_token}"
-            }
-    )
-    resp_json=response.json()
-
-   #print(resp_json)
-
-        
-   # if(resp_json['error']['status']==401):
-     #   return "Error 401: The access token expired"
-        
-    track_id=resp_json['item']['id']
-    track_name=resp_json['item']['name']
-    artists=resp_json['item']['artists']
-    artists_name = ', '.join([artist['name'] for artist in artists])
-    time=[0,0]
-    position=resp_json['progress_ms']  #miliseconds int 
-    time[0]=(int((position/1000)/60))
-    time[1]=(int((position/1000)%60))
-    pic=resp_json['item']['album']['images'][0]['url']
-    playing=resp_json['is_playing']
-    current_track_info = {
-            "id":track_id,
-            "name":track_name,
-            "artists":artists_name,
-            "position":time,
-            "picture":pic,
-            "is_playing":playing,
         }
+    )
+    print(response)
+    resp_json = response.json()
+   # response = Str(response)
+    # print(response.status_code)
+    # print(type(response))
+    global token
+    # print(token)
+    if(response.status_code != 200):
+
+        token = getAccessToken()
+        return 0
+
+    if(resp_json['currently_playing_type'] == 'track'):
+        track_id = resp_json['item']['id']
+        track_name = resp_json['item']['name']
+        artists = resp_json['item']['artists']
+        main_artist = resp_json['item']['artists'][0]['name']
+        artists_name = ', '.join([artist['name'] for artist in artists])
+        time = [0, 0]
+        position = resp_json['progress_ms']/1000
+        time[0] = (int((position)/60))
+        time[1] = (int((position) % 60))
+        pic = resp_json['item']['album']['images'][0]['url']
+        playing = resp_json['is_playing']
+        length = [0, 0]
+        length_raw = (resp_json['item']["duration_ms"])/1000
+        length[0] = (int((length_raw)/60))
+        length[1] = (int((length_raw) % 60))
+        # percent_complete=f"% {position*100/length_raw}"
+        current_track_info = {
+            "id": track_id,
+            "name": track_name,
+            "artists": artists_name,
+            "position": time,
+            "length": length,
+            "picture": pic,
+            "is_playing": playing,
+            "main_artist": main_artist,
+            # "% complete":percent_complete
+        }
+    else:
+        return 'Non-Track Playing?'
     return current_track_info
-            
 
-        
-                
-            
-#print(getAccessToken())
-#pprint(current_track('BQC5pBPma_7nzd9wjWBH8xqltAZag8hqTtXgqBydj6aWmm5EVVxegOczMlltrfmzc7QrJGYYD9GU2GAw8jC7gs_ZBvb-6IQeL-PzSiVNOHgELK2kOpUhImHpoTINvoCR0YQBEN-Lzg1eGIE2EywPnmBE4w'))
 
+# print(getAccessToken())
 def main():
-    token=getAccessToken()
-    listining_info=current_track(token)
-    pprint(listining_info,indent=4)
-    
+    global token
+    token = 0
+    while(True):
+        listining_info = current_track(token)
+        # print(token)
+        pprint(listining_info, indent=4)
+        if(listining_info != 0):
+            time.sleep(5)
+
+        # sql stuff
+
+    return listining_info
 
 
 main()
-    
