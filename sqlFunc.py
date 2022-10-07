bbd7997894a8504cbce87e3b14b16f462a613e3d
MMazenA <Jazenjirza@gmail.com>
2022-10-07 15:12:34 -0400

    exception handling and PEP8 corrections

--- sqlFunc.py
+++ sqlFunc.py
@@ -1,5 +1,4 @@
 """Requires mysql and privateInfo for authenticatio"""
-
 import mysql.connector
 import privateinfo
 
@@ -8,7 +7,7 @@ def data_insert(payload):
     """Inserts data into mySQL database."""
     # inserting for update counts [x,playcount,playtime]
     payload = payload+(payload[5],)+(payload[6],)+(payload[8],)
-    sql = '''INSERT INTO `current` 
+    sql = '''INSERT INTO `current`
   (`song_id`, `song_name`, `artists`, `primary_artist`, `song_length`, `total_play_count`, `current_play_time`, `pic_link`,`rowid`) 
   VALUES('%s',"%s","%s","%s",'%s','%s','%s',"%s","%s") 
   ON DUPLICATE KEY UPDATE
@@ -20,45 +19,44 @@ def data_insert(payload):
             host=privateinfo.sql_host(),
             user=privateinfo.sql_user(),
             password=privateinfo.sql_pass(),
-            database="d"
-        )
+            database=privateinfo.sql_db())
         cur = mydb.cursor()
-    except:
-        print("Unable to connect to database")
-    try:
-        cur.execute(sql)
-        mydb.commit()
-    except:
-        print("Insert Error: Values: ", payload)
-        print(sql)
-    cur.close()
-    mydb.close()
+        try:
+            cur.execute(sql)
+            mydb.commit()
+            cur.close()
+            mydb.close()
+        except mysql.connector.Error as sqlerr1:
+            print("Insert Error: Values: ", payload)
+            print(sqlerr1)
+            print(sql)
+    except mysql.connector.Error as sqlerr:
+        print("Unable to connect to database: ", sqlerr)
 
 
 def last_row():
+    """Retrive last row from database"""
     row = ""
     try:
         mydb = mysql.connector.connect(
             host=privateinfo.sql_host(),
             user=privateinfo.sql_user(),
             password=privateinfo.sql_pass(),
-            database="d"
+            database=privateinfo.sql_db()
         )
         cur = mydb.cursor()
-    except:
-        print("ERROR: Unable to connect to database")
-    sql = "SELECT * FROM `current` ORDER BY rowid DESC LIMIT 1"
+        try:
+            sql = "SELECT * FROM `current` ORDER BY rowid DESC LIMIT 1"
+            cur.execute(sql)
+            row = cur.fetchone()
+            cur.close()
+            mydb.close()
 
-    try:
-        cur.execute(sql)
-        row = cur.fetchone()
-        # print(row)
+        except mysql.connector.Error as err1:
+            print("ERROR: Unable to retrive last row", err1)
+    except mysql.connector.Error as err:
+        print("ERROR: Unable to connect to database", err)
 
-    except:
-        print("ERROR: Unable to retrive last row")
-
-    cur.close()
-    mydb.close()
     return row
 
 
@@ -67,42 +65,41 @@ def reset_rows():
     row = ""
     try:
         mydb = mysql.connector.connect(
-            host=privateinfo.sqlHost(),
-            user=privateinfo.sqlUser(),
-            password=privateinfo.sqlPass(),
-            database="d"
+            host=privateinfo.sql_host(),
+            user=privateinfo.sql_user(),
+            password=privateinfo.sql_pass(),
+            database=privateinfo.sql_db()
         )
         mydb2 = mysql.connector.connect(
             host=privateinfo.sql_host(),
             user=privateinfo.sql_user(),
             password=privateinfo.sql_pass(),
-            database="d"
+            database=privateinfo.sql_db()
         )
         cur = mydb.cursor()
-        curEdit = mydb2.cursor()
-    except:
-        print("ERROR: Unable to connect to database")
-    sql = "SELECT * FROM `current` ORDER BY rowid ASC"
+        cur_edit = mydb2.cursor()
 
-    try:
-        cur.execute(sql)
-        row = cur.fetchone()
-        count = 0
-
-        while row is not None:
-            sql = """INSERT INTO `current` (`song_id`) VALUES ('%s') 
-            ON DUPLICATE KEY UPDATE
-            `rowid`='%s'""" % (row[0], count)
-            curEdit.execute(sql)
-            mydb2.commit()
-            count = count+1
+        try:
+            sql = "SELECT * FROM `current` ORDER BY rowid ASC"
+            cur.execute(sql)
             row = cur.fetchone()
+            count = 0
 
-    except:
-        print("Unable to resetRows")
-        print(sql)
+            while row is not None:
+                sql = """INSERT INTO `current` (`song_id`) VALUES ('%s')
+                ON DUPLICATE KEY UPDATE
+                `rowid`='%s'""" % (row[0], count)
+                cur_edit.execute(sql)
+                mydb2.commit()
+                count = count+1
+                row = cur.fetchone()
+                cur.close()
+                mydb.close()
+                cur_edit.close()
+                mydb2.close()
 
-    cur.close()
-    mydb.close()
-    curEdit.close()
-    mydb2.close()
+        except mysql.connector.Error as err1:
+            print("Unable to resetRows", err1)
+            print(sql)
+    except mysql.connector.Error as err:
+        print("ERROR: Unable to connect to database", err)
