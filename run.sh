232bcba09a54c80df4d73c8b21779e7adfb10575
MMazenA <Jazenjirza@gmail.com>
2022-10-23 05:31:47 -0400

    fixed logging

--- run.sh
+++ run.sh
@@ -2,13 +2,9 @@
 echo "Terminating any active sessions"
 sh ./terminate.sh
 
-screen -L -Logfile /logs/api.log -dmS api python3 ./t/api.py  
+screen -L -Logfile logs/api.log -dmS api python3 ./t/api.py  
 sleep 5
-screen -L -Logfile /logs/tracker.log -dmS sptfy python3 ./spotifyTracker/__main__.py 
+screen -L -Logfile logs/tracker.log -dmS sptfy spotifyTracker
 echo "API and Tracker active"
 screen -list
 sh ./announce.sh
-
-
-
-
