1c91518621770e7eb6afe63bc108639a43f7fc09
MMazenA <Jazenjirza@gmail.com>
2022-10-27 11:52:00 -0400

    dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 """
 Spotify Project.
 
@@ -8,19 +7,21 @@ Mazen Mirza <jazenjirza@gmail.com>
 from setuptools import setup, find_packages
 
 setup(
-    name='spotifyTracker',
-    version='0.1.0',
+    name="spotifyTracker",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click',
-        'requests',
-        'setuptools',
+        "click",
+        "requests",
+        "setuptools",
+        "mysql.connector",
+        "Flask",
+        "Flask-RESTful ",
+        "Flask-Limiter",
+        "flask_sse",
+        "Flask-WTF",
     ],
-    python_requires='>=3.8',
-    entry_points={
-        'console_scripts': [
-            'spotifyTracker = spotifyTracker.__main__:main'
-        ]
-    },
+    python_requires=">=3.8",
+    entry_points={"console_scripts": ["spotifyTracker = spotifyTracker.__main__:main"]},
 )
