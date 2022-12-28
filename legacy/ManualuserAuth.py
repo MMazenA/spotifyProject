8cae319490aa96b0e14c3b99b73812053e9e62aa
MMazenA <Jazenjirza@gmail.com>
2022-12-28 10:08:09 -0500

    cleaning up

---
+++ legacy/ManualuserAuth.py
@@ -0,0 +1,37 @@
+from flask import Flask, redirect, request
+from urllib import request
+import privateinfo   # file containing client codes
+
+
+# client id and secret
+client_id = privateinfo.client_id()
+secret_id = privateinfo.secret_id()
+
+
+# authentication URL
+userauthenticate = (
+    "response_type=code" +
+    '&client_id='+client_id +
+    "&scope=user-read-currently-playing%20user-read-playback-state%20user-read-playback-position"
+    "&redirect_uri=http://127.0.0.1:5000/home/"
+)
+
+
+# authentication using flask for browser
+app = Flask(__name__)
+
+
+def authenticate():
+
+    @app.route("/")
+    def spotify():
+        return redirect('https://accounts.spotify.com/authorize?'+userauthenticate)
+
+    @app.route("/home/")
+    def home():
+        return ('hi')
+
+    app.run(host='0.0.0.0')
+
+
+authenticate()
